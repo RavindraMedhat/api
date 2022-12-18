a25b44974cfa2f72ffb7bb2c0ad834b0e22e1db0
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2022-12-18 17:17:50 +0530

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 7485;
+const appdata = require("./data.json");
+
+app.get("/", (req, res) => {
+    res.send("hello i am reavindra");
+});
+
+app.get("/data", (req, res) => {
+    res.send(appdata);
+});
+
+app.listen(port, () => {
+    console.log("i am ravi");
+});
