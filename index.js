b1df90113892786ed9ac10045038f2ee1f02e10f
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-01-07 09:15:52 +0530

    user by id

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 require("./database/conn")
-// const port = process.env.PORT || 1234;
+
 const User = require("./models/user")
-const cors = require("cors");
 const app = express();
 app.use(express.json());
 
@@ -13,31 +12,48 @@ app.get("/", (req, res) => {
     res.send("hello i am ravindrasinh");
 });
 
+// add user
 app.post("/user", async (req, res) => {
 
     try {
         const reqdata = new User(req.body);
         const createUser = await reqdata.save();
         res.status(201).send(createUser);
-        // console.log("user is add");
     } catch (e) {
         console.log("error :- ", e);
         res.status(400).send(e);
     }
 })
 
+// get list of users
 app.get("/users", async (req, res) => {
-    // res.send(appdata);
     try {
         const data = await User.find();
         res.send(data);
     } catch (e) {
         console.log("error :- ", e);
         res.status(400).send(e);
-        // res.send(appdata);
     }
 
 })
+// get user data by id
+app.get("/user/:id", async (req, res) => {
+
+    try {
+        const _id = req.params.id;
+        const data = await User.findById(_id);
+        console.log(data);
+        if (!data)
+            return res.status(404).send();
+        else
+            res.send(data);
+
+    } catch (e) {
+        console.log("error :- ", e);
+        res.status(400).send(e);
+    }
+})
+
 app.get("/data", async (req, res) => {
     res.send(appdata);
 });
