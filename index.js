d36b9443ec6c8a0148ce916fc61546f2d1cedd0b
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-01-07 07:46:25 +0530

    try3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/data", async (req, res) => {
         res.send(data);
     } catch (e) {
         console.log("error :- ", e);
-        // res.status(400).send(e);
+        res.status(400).send(e);
         res.send(appdata);
     }
 });
