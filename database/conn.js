19920bc0540e1f8c7bcb968a3166d1ceca805331
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-04-10 14:16:01 +0530

    change password of database

--- database/conn.js
+++ database/conn.js
@@ -1,6 +1,6 @@
 const mongose = require("mongoose");
 
-mongose.connect("mongodb+srv://test:test@cluster0.3snq0fm.mongodb.net/RVCL_DB?retryWrites=true&w=majority"
+mongose.connect("mongodb+srv://test:74857485@cluster0.3snq0fm.mongodb.net/RVCL_DB?retryWrites=true&w=majority"
     , {
         useNewUrlParser: true,
         // useCreateIndex: true,
