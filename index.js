d3a8d3c7fd0deed95509e8faf047a7dfa951eed6
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-04-14 14:35:40 +0530

    login problem try 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes")
 const appdata = require("./data.json");
+const User = require("../models/user");
 const mongose = require("mongoose");
 
 const app = express();
@@ -38,6 +39,7 @@ app.use('/user', userRoutes);
 app.post("/LoginReq", (req, res) => {
 
     const reqData = req.body;
+
     User.find(reqData)
         .then((data) => {
             if (data.length == 0) {
