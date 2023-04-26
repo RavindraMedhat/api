d95e8327c1362cb907213fdc366d5af682735aee
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-04-26 21:58:41 +0530

    search for customer by name

--- controller/customerController.js
+++ controller/customerController.js
@@ -27,7 +27,32 @@ const customer_list = async (req, res) => {
 
 }
 
+const customer_search = (req, res) => {
+    const name = req.params.name;
+    customer.find({ name: { $regex: name, $options: 'i' } }).then((customer_data) => {
+        return res.status(200).json({ success: true, customer_data });
+
+    }).catch((e) => {
+        return res.status(201).json({ success: false, message: e.message });
+    });
+}
+
+const customer_sort_last_10 = async (req, res) => {
+
+    const customer_data = await customer.find()
+        .sort({ createdAt: -1 })
+        .limit(10).then((customer_data) => {
+            return res.status(200).json({ success: true, customer_data });
+
+        }).catch((e) => {
+            return res.status(201).json({ success: false, message: e.message });
+        });
+
+}
+
 module.exports = {
     customer_add,
-    customer_list
+    customer_list,
+    customer_search,
+    customer_sort_last_10
 }
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { customer_add, customer_list } = require("../controller/customerController");
+const { customer_add, customer_list, customer_search, customer_sort_last_10 } = require("../controller/customerController");
 const router = express.Router();
 
 router.post('/add', customer_add);
 
 router.get('/list', customer_list);
+
+router.get('/search/:name', customer_search);
+
+router.get('/sort_last_10', customer_sort_last_10);
+
 
 module.exports = router;
