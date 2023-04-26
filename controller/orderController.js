447b783bb1d68c289e2b86c9e17af6928820eda1
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-04-26 16:01:32 +0530

    all order data by order_id

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,3 +1,5 @@
+const customer = require("../models/customer");
+const garmentsType = require("../models/garmentsType");
 const order = require("../models/order");
 
 const order_add = async (req, res) => {
@@ -27,7 +29,47 @@ const order_list = async (req, res) => {
 
 }
 
+const order_ByOrderId = async (req, res) => {
+
+    order.find({ order_id: req.params.id })
+        .then((order_data) => {
+            if (order_data.length == 0)
+                return res.status(200).json({ success: false, message: "no data found" });
+            else {
+                customer.findById(order_data[0].customer_id)
+                    .then((customer_data) => {
+                        if (customer_data.length == 0)
+                            return res.status(200).json({ success: false, message: "no data found" });
+                        else {
+                            garmentsType.findById(order_data[0].garment_type_id)
+                                .then((garment_type_data) => {
+                                    if (garment_type_data.length == 0)
+                                        return res.status(200).json({ success: false, message: "no data found" });
+                                    else {
+                                        return res.status(200).json({ success: true, Data: { order_data, customer_data, garment_type_data } });
+                                    }
+                                }).catch((e) => {
+                                    console.log("error :- ", e);
+                                    return res.status(400).json(e);
+                                })
+                        }
+                    })
+                    .catch((e) => {
+                        console.log("error :- ", e);
+                        return res.status(400).json(e);
+                    })
+
+            }
+        })
+        .catch((e) => {
+            console.log("error :- ", e);
+            return res.status(400).json(e);
+        });
+
+}
+
 module.exports = {
     order_add,
-    order_list
+    order_list,
+    order_ByOrderId
 }
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { order_add, order_list } = require("../controller/orderController");
+const { order_add, order_list, order_ByOrderId } = require("../controller/orderController");
 const router = express.Router();
 
 router.post('/add', order_add);
 
 router.get('/list', order_list);
+
+router.get('/ByOrderId/:id', order_ByOrderId);
 
 module.exports = router;
