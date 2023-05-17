df2517fbac08dd9caad3fea525eaa049ed91f74a
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-05-17 13:11:08 +0530

    getRecords to getRecord

--- routes/garmentTrackingRoutes.js
+++ routes/garmentTrackingRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { addRecord, getRecords, updateReceivingDate } = require("../controller/garmentTrackingControler");
+const { addRecord, getRecords, updateReceivingDate ,getRecord} = require("../controller/garmentTrackingControler");
 const router = express.Router();
 
 router.post('/addRecord', addRecord);
 
 router.put('/updateRecord', updateReceivingDate);
 
-router.get('/ByGarmentTrackingId/:garmentTrackingId', getRecords);
+router.get('/ByGarmentTrackingId/:garmentTrackingId', getRecord);
 
 router.get('/list', getRecords);
 
