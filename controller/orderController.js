a94e81427f8a56d3f41086d9a856b58ef7e6f2ea
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-05-17 12:45:19 +0530

    status code change

--- controller/orderController.js
+++ controller/orderController.js
@@ -86,17 +86,17 @@ const order_ByOrderId = async (req, res) => {
     order.find({ order_id: req.params.id })
         .then((order_data) => {
             if (order_data.length == 0)
-                return res.status(200).json({ success: false, message: "no data found" });
+                return res.status(201).json({ success: false, message: "no data found" });
             else {
                 customer.findById(order_data[0].customer_id)
                     .then((customer_data) => {
                         if (customer_data.length == 0)
-                            return res.status(200).json({ success: false, message: "no data found" });
+                            return res.status(201).json({ success: false, message: "no data found" });
                         else {
                             garmentsType.findById(order_data[0].garment_type_id)
                                 .then((garment_type_data) => {
                                     if (garment_type_data.length == 0)
-                                        return res.status(200).json({ success: false, message: "no data found" });
+                                        return res.status(201).json({ success: false, message: "no data found" });
                                     else {
                                         console.log({ success: true, Data: { order_data: order_data[0], customer_data, garment_type_data } });
                                         return res.status(200).json({ success: true, Data: { order_data: order_data[0], customer_data, garment_type_data } });
