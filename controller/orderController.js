eca23316bd48eab4fc1191a028b6ca0c2cc8f7f1
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-05-26 08:03:17 +0530

    solve problem

--- controller/orderController.js
+++ controller/orderController.js
@@ -122,13 +122,13 @@ const order_ByOrderId = async (req, res) => {
                         if (customer_data.length == 0)
                             return res.status(201).json({ success: false, message: "no data found" });
                         else {
-                            garmentsType.findById(order_data[0].garment_type_id)
+                            garmentsType.findById(order_data.garment_type_id)
                                 .then((garment_type_data) => {
                                     if (garment_type_data.length == 0)
                                         return res.status(201).json({ success: false, message: "no data found" });
                                     else {
                                         // console.log({ success: true, Data: { order_data: order_data[0], customer_data, garment_type_data } });
-                                        return res.status(200).json({ success: true, Data: { order_data: order_data[0], customer_data, garment_type_data } });
+                                        return res.status(200).json({ success: true, Data: { order_data: order_data, customer_data, garment_type_data } });
                                     }
                                 }).catch((e) => {
                                     console.log("error :- ", e);
