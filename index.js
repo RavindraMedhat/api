5587e447735091aa1acecba51bc723039b1589e1
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-08-03 09:26:18 +0530

    try 3

--- index.js
+++ index.js
@@ -39,18 +39,10 @@ mongose.connect("mongodb+srv://test:74857485@cluster0.3snq0fm.mongodb.net/RVCL_D
     console.log(e);
 });
 
-const allowedOrigins = [''];
-
-// Enable CORS for specified origins
 app.use(cors({
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
